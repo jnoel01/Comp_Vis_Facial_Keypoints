d0095a9ec52a5c67123ef61fd02364cba369de2f
Jessica Noel <JNoel2@its.jnj.com>
2023-11-21 12:49:31 -0500

    Mod 2

--- models.py
+++ models.py
@@ -12,26 +12,50 @@ class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        
+
         ## TODO: Define all the layers of this CNN, the only requirements are:
         ## 1. This network takes in a square (same width and height), grayscale image as input
         ## 2. It ends with a linear layer that represents the keypoints
         ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
-        
+
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         self.conv1 = nn.Conv2d(1, 32, 5)
-        
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.conv4 = nn.Conv2d(128, 256, 3)
+        self.fc1 = nn.Linear(256 * 12 * 12, 512)
+        self.fc2 = nn.Linear(512, 136)
+
+        self.dropout = nn.Dropout(0.3)
+
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        
 
-        
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
-        
-        
-        # a modified x, having gone through all the layers of your model, should be returned
-        return x
+       # Apply convolutional and pooling layers
+      x = self.pool(F.relu(self.conv1(x)))
+      x = self.pool(F.relu(self.conv2(x)))
+      x = self.pool(F.relu(self.conv3(x)))
+      x = self.pool(F.relu(self.conv4(x)))
+
+      # Print the size before flattening
+      print("Size before flattening:", x.size())
+
+      # Flatten the feature maps
+      x = x.view(x.size(0), -1)
+
+      # Print the size after flattening
+      print("Size after flattening:", x.size())
+
+      # Apply fully connected layers
+      x = F.relu(self.fc1(x))
+      x = self.dropout(x)
+      x = self.fc2(x)
+
+      # a modified x, having gone through all the layers of your model, should be returned
+      return x
